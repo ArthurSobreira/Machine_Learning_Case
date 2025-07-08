f8ef9999d653f6f66af8accb1fc26d82729d00f3
Arthur Sobreira <tutusobreira@gmail.com>
2025-07-08 00:20:05 -0300

    feat: Update PDF reader library

--- src/core/pdf_reader.py
+++ src/core/pdf_reader.py
@@ -1,11 +1,16 @@
 from PyPDF2 import PdfReader
+import pdfplumber
 
 def extract_text_from_pdf(file_stream) -> str:
-  # """Recebe um stream de PDF e retorna o texto extraído"""
-  reader = PdfReader(file_stream)
+  """Recebe um stream de PDF e retorna o texto extraído"""
+  # reader = PdfReader(file_stream)
   text = ""
 
-  for page in reader.pages:
-    text += page.extract_text() or ""
+  # for page in reader.pages:
+  #   text += page.extract_text() or ""
+
+  with pdfplumber.open(file_stream) as pdf:
+    for page in pdf.pages:
+      text += page.extract_text() + "\n"
 
   return text.strip()
