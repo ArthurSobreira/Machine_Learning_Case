6719a219932ff77e268af8b13858785ff4c41003
Arthur Sobreira <tutusobreira@gmail.com>
2025-07-08 19:09:24 -0300

    feat: Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from core.pdf_reader import extract_text_from_pdf
 
+
 def main():
-  pdf_path = "/Users/arthursobreira/repos/case_tractian/case_files/LB5001.pdf"
+  # pdf_path = "/Users/arthursobreira/repos/case_tractian/case_files/LB5001.pdf"
+  pdf_path = "./LB5001.pdf" # caminho dentro do container
   
   try:
     with open(pdf_path, "rb") as pdf_file:
