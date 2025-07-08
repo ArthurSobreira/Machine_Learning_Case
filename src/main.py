560c69c212f20fcf972e03a06b4effed8bb5894f
Arthur Sobreira <tutusobreira@gmail.com>
2025-07-08 00:18:00 -0300

    feat: Update main with extract_text_from_pdf tests

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from core.pdf_reader import extract_text_from_pdf
+
+def main():
+  pdf_path = "/Users/arthursobreira/repos/case_tractian/case_files/LB5001.pdf"
+  
+  try:
+    with open(pdf_path, "rb") as pdf_file:
+      extracted_text = extract_text_from_pdf(pdf_file)
+      print("conteudo: ")
+      print(extracted_text)
+  except FileNotFoundError:
+    print(f"erro: {pdf_path}")
+  except Exception as e:
+    print(f"erro: {e}")
+
+if __name__ == "__main__":
+  main()
