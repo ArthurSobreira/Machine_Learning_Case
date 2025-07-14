f13edd6e331fc3576f991e477b072dbfdbd5cb48
Arthur Sobreira <tutusobreira@gmail.com>
2025-07-14 02:05:39 -0300

    feat: Update QAHandler class

---
+++ src/core/qa_handler/__init__.py
@@ -0,0 +1,61 @@
+from sentence_transformers import SentenceTransformer
+from .loader import load_from_disk
+from .chunk_selector import select_top_k_chunks
+# from .llm_caller import call_model
+from typing import List
+
+
+class QAHandler:
+  def __init__(self):
+    self._chunks: List[str] = []
+    self._embeddings: List[List[float]] = []
+    self._question_embedding: List[float] = []
+    self._model_name: str = "gpt-3.5"
+    self._embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+  def answer_question(self, question: str):
+    """
+    Executes the complete question-answering pipeline.
+
+    Args:
+      question (str): The user's question.
+      model (str): The name of the model to use for answering the question.
+    """
+    if not question or question.strip() == "":
+      raise ValueError("Question cannot be empty.")
+
+    self._generate_question_embedding(question)
+    self._load_from_disk()
+
+    # print(f"Question: {question}")
+    # print(f"chunks loaded: {len(self._chunks)}, {self._chunks[:3]}")
+    # print(f"embeddings loaded: {len(self._embeddings)}, {self._embeddings[:3]}")
+
+    if not self._chunks or not self._embeddings:
+      raise RuntimeError("No chunks or embeddings loaded. Call load_from_disk() first.")
+
+    top_chunks = select_top_k_chunks(self._question_embedding, self._embeddings, self._chunks)
+    context = "\n".join(top_chunks)
+
+    # answer = call_model(
+    #   question=question, 
+    #   context=context,
+    #   model_name=self._model_name
+    # )
+  
+    answer = "This is a dummy answer. Replace with actual model call."
+  
+    return answer, top_chunks
+
+  def _load_from_disk(self) -> None:
+    """Load processed chunks and embeddings from disk."""
+    self._chunks, self._embeddings = load_from_disk()
+
+  def _generate_question_embedding(self, question: str) -> None:
+    """Generate an embedding for the user's question."""
+    model = self._embedding_model
+    self._question_embedding = model.encode(
+      question,
+      show_progress_bar=True,
+      convert_to_tensor=False
+    ).tolist()
